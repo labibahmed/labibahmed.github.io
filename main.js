b830d043c47237cb4c477ef5608d47e38b1ad736
labibahmed <12345678labib@gmail.com>
2022-01-17 00:48:57 -0500

    Fullpage scrolling but scrollpsy is not working

--- main.js
+++ main.js
@@ -17,4 +17,41 @@ $(function () {
     $.get("parts/Projects.html", function (data) {
         $("#Projects").append(data);
     });
+    $('#pagepiling').pagepiling({
+        menu: '#myMenu',
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: ['Hero', 'Skills-Edu', 'Experience', 'Projects'],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: true,
+        loopTop: true,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['Home', 'Qualifications', 'Expereince', 'Projects']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.sec',
+        animateAnchor: false,
+
+        //events
+        onLeave: function (index, nextIndex, direction) { },
+        afterLoad: function (anchorLink, index) { },
+        afterRender: function () { },
+    });
+
+
 });
+
+function onClick() {
+    $('.navbar-collapse a').on("click", function () {
+        $(".navbar-collapse").collapse('hide');
+    });
+}
