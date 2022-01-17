356cdf0870b7b63c1f3eff935998b04140fe10f5
labibahmed <12345678labib@gmail.com>
2022-01-17 13:36:39 -0500

    started animations

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+var gsapAni = gsap.timeline();
 $(function () {
-    $.get("parts/Nav.html", function (data) {
+    /*$.get("parts/Nav.html", function (data) {
         $("body").prepend(data);
-    });
+    });*/
     $.get("parts/Hero.html", function (data) {
-        $("#Hero").append(data);
+        $("#Home").append(data);
     });
     $.get("parts/Skills.html", function (data) {
         $("#Skills").append(data);
@@ -18,12 +19,12 @@ $(function () {
         $("#Projects").append(data);
     });
     $('#pagepiling').pagepiling({
-        menu: '#myMenu',
+        menu: '#menu',
         direction: 'vertical',
         verticalCentered: true,
         sectionsColor: [],
-        anchors: ['Hero', 'Skills-Edu', 'Experience', 'Projects'],
-        scrollingSpeed: 700,
+        anchors: ['Home', 'Qualifications', 'Experience', 'Projects'],
+        scrollingSpeed: 600,
         easing: 'swing',
         loopBottom: true,
         loopTop: true,
@@ -32,22 +33,68 @@ $(function () {
             'textColor': '#000',
             'bulletsColor': '#000',
             'position': 'right',
-            'tooltips': ['Home', 'Qualifications', 'Expereince', 'Projects']
+            'tooltips': ['Home', 'Qualifications', 'Experience', 'Projects']
         },
         normalScrollElements: null,
         normalScrollElementTouchThreshold: 5,
         touchSensitivity: 5,
         keyboardScrolling: true,
-        sectionSelector: '.sec',
+        sectionSelector: '.section',
         animateAnchor: false,
 
         //events
-        onLeave: function (index, nextIndex, direction) { },
-        afterLoad: function (anchorLink, index) { },
+        onLeave: function (index, nextIndex, direction) {
+            switch (index) {
+                case 1:
+                    $(".home-list").removeClass("active");
+                    gsapAni
+                        .to("#Home", { duration: 0, opacity: 0 });
+                    break;
+                case 2:
+                    $(".qual-list").removeClass("active");
+                    gsapAni
+                        .to("#Qualifications", { duration: 0, opacity: 0 });
+                    break;
+                case 3:
+                    $(".exp-list").removeClass("active");
+                    gsapAni
+                        .to("#Experience", { duration: 0, opacity: 0 });
+                    break;
+                case 4:
+                    $(".pro-list").removeClass("active");
+                    gsapAni
+                        .to("#Projects", { duration: 0, opacity: 0 });
+                    break;
+            }
+
+        },
+        afterLoad: function (anchorLink, index) {
+            switch (index) {
+                case 1:
+                    $(".home-list").addClass("active");
+                    gsapAni
+                        .fromTo("#Home", 2, { opacity: 0 }, { opacity: 100 });
+
+                    break;
+                case 2:
+                    $(".qual-list").addClass("active");
+                    gsapAni
+                        .fromTo("#Qualifications", 2, { opacity: 0 }, { opacity: 100 });
+                    break;
+                case 3:
+                    $(".exp-list").addClass("active");
+                    gsapAni
+                        .fromTo("#Experience", 2, { opacity: 0 }, { opacity: 100 });
+                    break;
+                case 4:
+                    $(".pro-list").addClass("active");
+                    gsapAni
+                        .fromTo("#Projects", 2, { opacity: 0 }, { opacity: 100 });
+                    break;
+            }
+        },
         afterRender: function () { },
     });
-
-
 });
 
 function onClick() {
